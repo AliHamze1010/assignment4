ac28dd1fa7bad33605562e1e19440e351959ef2c
ali <alibhamze@hotmail.com>
2023-04-11 12:15:41 -0400

    Add project files.

---
+++ assignment4/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace assignment4.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ assignment4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="assignment4.WebApiApplication" Language="C#" %>
