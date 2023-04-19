512d13d5a2e5f7c6669441f407eb0060fa6ba0b1
ali <alibhamze@hotmail.com>
2023-04-19 10:41:51 -0400

    1st commit

---
+++ assignment4/Models/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace assignment4.Models
+{
+    public class Teacher
+    {
+
+        //Teacher columns
+        public int TeacherId;
+        public string TeacherFname;
+        public string TeacherLname;
+        public string TeacherEmployeeNumber;
+        public DateTime TeacherHireDate;
+        public decimal TeacherSalary;
+
+        public Teacher() { }
+    }
+}
